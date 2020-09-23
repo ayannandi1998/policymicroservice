8caf3b4029e8e3bc317059ebc6a1597b0d7b6469
ayannandi1998 <66714958+ayannandi1998@users.noreply.github.com>
2020-09-23 19:16:40 +0530

    Add project files.

---
+++ Controllers/policyController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using policymicroservice.Models;
+using policymicroservice.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace policymicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class policyController : ControllerBase
+    {
+        // GET: api/<memberController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            memberpolicyrepo ob = new memberpolicyrepo();
+            return Ok(ob.fun());
+        }
+        [HttpGet("{memberid}")]
+        public int getpolid(int memberid)
+        {
+            memberpolicyrepo ob = new memberpolicyrepo();
+            int policyid = ob.receivepolicyid(memberid);
+            return policyid;
+        }
+        [HttpGet("{policyid}/{memberid}")]
+        public List<int> GetEligibleBenefits(int policyid, int memberid)//run in browser1 2 100 200 300 400
+        {
+            memberpolicyrepo o = new memberpolicyrepo();
+            providerpolicyrepo res = new providerpolicyrepo();
+            List<int> cob = new List<int>();
+            List<int> f = new List<int>();
+            cob = o.gethosloc(memberid); //hosp loc
+            f = res.getbenefits(cob);
+            return f;
+        }
+        [HttpGet("{policyid}/{memberid}/{benefitid}")]
+
+
+
+        // ambulance medical operation staying
+        //    1         2        3         4      ----> for user
+        //    0         1         2        3      ----> as per my list
+        public int getEligibleClaimAmount(int policyid,int memberid,int benefitid)
+        {
+            if (benefitid == 0)
+            {
+                policyrepo x = new policyrepo();
+                int y = x.givepolicy(policyid);
+                return 2*y;
+            }
+            else
+            {
+                List<int> r = new List<int>();
+                r = GetEligibleBenefits(policyid, memberid);//[{ 400,300,200,300}]
+                return r[benefitid-1];
+                              
+            }
+        }
+    }
+}
